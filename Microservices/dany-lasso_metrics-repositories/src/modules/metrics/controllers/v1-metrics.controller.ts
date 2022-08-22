676103a170e5828ffcd46141827c720faaf7e05b
dlassoa <dlasso@ec.krugercorp.com>
2022-08-22 16:28:59 -0500

    add controller and services

---
+++ Microservices/dany-lasso_metrics-repositories/src/modules/metrics/controllers/v1-metrics.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('v1-metrics')
+export class V1MetricsController {}
--- Microservices/dany-lasso_metrics-repositories/src/modules/metrics/metrics.module.ts
+++ Microservices/dany-lasso_metrics-repositories/src/modules/metrics/metrics.module.ts
@@ -3,8 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 //Entities
 import { Metric } from './entities/metric.entity';
+import { V1MetricsController } from './controllers/v1-metrics.controller';
+import { FindMetricsService } from './service/find-metrics.service';
+import { SaveMetricsService } from './service/save-metrics.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Metric])],
+  controllers: [V1MetricsController],
+  providers: [FindMetricsService, SaveMetricsService],
 })
 export class MetricsModule {}

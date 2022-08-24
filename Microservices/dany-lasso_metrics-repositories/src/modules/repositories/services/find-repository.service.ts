2e7d4d85a495bb995b94e9647073ad5a843a2d1d
dlassoa <dlasso@ec.krugercorp.com>
2022-08-24 12:01:18 -0500

    Added feature/find all repositories
    Changes:
    	modified:   src/modules/repositories/services/find-repository.service.ts

--- Microservices/dany-lasso_metrics-repositories/src/modules/repositories/services/find-repository.service.ts
+++ Microservices/dany-lasso_metrics-repositories/src/modules/repositories/services/find-repository.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Repo } from '../entities/repositorie.entity';
 
 @Injectable()
-export class FindRepositoryService {}
+export class FindRepositoryService {
+  constructor(
+    @InjectRepository(Repo)
+    private repoRepository: Repository<Repo>,
+  ) {}
+
+  /**
+   * Find all repositories with your metrics
+   * @returns Promise<Repo[]>
+   */
+  findAll() {
+    return this.repoRepository.find({
+      relations: ['metric'],
+    });
+  }
+}

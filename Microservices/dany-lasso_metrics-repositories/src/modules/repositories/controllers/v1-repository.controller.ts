c6822c45770527ddd72d9eaae5a11564ca8e41e0
dlassoa <dlasso@ec.krugercorp.com>
2022-08-24 12:28:13 -0500

    Expose APIs of repository
    Changes:
    	modified:   src/modules/repositories/controllers/v1-repository.controller.ts
    	modified:   src/modules/tribes/tribes.module.ts
    	deleted:    src/settings/database/migrations/1661191581362-scripts.ts
    	deleted:    src/settings/database/migrations/1661191759264-scripts.ts
    	new file:   src/settings/database/migrations/1661361357951-scripts.ts

--- Microservices/dany-lasso_metrics-repositories/src/modules/repositories/controllers/v1-repository.controller.ts
+++ Microservices/dany-lasso_metrics-repositories/src/modules/repositories/controllers/v1-repository.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateRepositoryRequestDto } from '../dtos/repositories-requests.dto';
+import { Repo } from '../entities/repositorie.entity';
+import { FindRepositoryService } from '../services/find-repository.service';
+import { SaveRepositoryService } from '../services/save-repository.service';
+@ApiTags('Repositories')
+@Controller('/api/v1/repository')
+export class V1RepositoryController {
+  constructor(
+    private findRepositoryService: FindRepositoryService,
+    private saveRepositoryService: SaveRepositoryService,
+  ) {}
 
-@Controller('v1-repository')
-export class V1RepositoryController {}
+  @ApiOperation({ summary: 'Get all Repositories' })
+  @Get()
+  findAll(): Promise<Repo[]> {
+    return this.findRepositoryService.findAll();
+  }
+
+  @ApiOperation({ summary: 'Save Repository' })
+  @Post()
+  save(@Body() body: CreateRepositoryRequestDto): Promise<Repo> {
+    return this.saveRepositoryService.save(body);
+  }
+}

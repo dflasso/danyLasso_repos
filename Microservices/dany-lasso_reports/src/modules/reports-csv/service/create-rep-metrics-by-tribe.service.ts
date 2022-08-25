bf6ee22bb521ceaab8ebea6b7b1252900134afd4
dlassoa <dlasso@ec.krugercorp.com>
2022-08-25 11:20:07 -0500

    Completed Report CSV about metrics of repositories by tribe id
    Changes:
    	new file:   Microservices/dany-lasso_reports/src/middleware/clients-ws/metrics-repos-microservice/dtos/apis-response.dto.ts
    	new file:   Microservices/dany-lasso_reports/src/modules/reports-csv/constants/headers-reportes.ts
    	modified:   Microservices/dany-lasso_reports/src/modules/reports-csv/controller/v1-reports-csv.controller.ts
    	new file:   Microservices/dany-lasso_reports/src/modules/reports-csv/serializer/dto-to-csv-format.serializer.ts
    	modified:   Microservices/dany-lasso_reports/src/modules/reports-csv/service/create-rep-metrics-by-tribe.service.ts
    	new file:   Microservices/dany-lasso_reports/test.csv

--- Microservices/dany-lasso_reports/src/modules/reports-csv/service/create-rep-metrics-by-tribe.service.ts
+++ Microservices/dany-lasso_reports/src/modules/reports-csv/service/create-rep-metrics-by-tribe.service.ts
@@ -1,11 +1,14 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
+import { Readable } from 'stream';
+import { ApiMetricsByTribeResponse } from '../../../middleware/clients-ws/metrics-repos-microservice/dtos/apis-response.dto';
 import { ApiMetricsByTribeService } from '../../../middleware/clients-ws/metrics-repos-microservice/services/api-metrics-by-tribe.service';
+import { convertFromApiMetricsByTribeResponseToCsvAsString } from '../serializer/dto-to-csv-format.serializer';
 
 @Injectable()
 export class CreateRepMetricsByTribeService {
   constructor(private apiMetricsByTribeService: ApiMetricsByTribeService) {}
 
-  generate(idTribe: number) {
+  generate(idTribe: number): Promise<Readable> {
     return new Promise((resolve, reject) => {
       this.apiMetricsByTribeService
         .consumeApi(idTribe)
@@ -14,11 +17,19 @@ export class CreateRepMetricsByTribeService {
             response.status >= HttpStatus.OK &&
             response.status <= HttpStatus.AMBIGUOUS
           ) {
-            resolve(response.data);
+            const fileStream = this.createFile(response.data);
+            resolve(fileStream);
           } else {
             reject('Unexpected error consuming API find metrics by tribes');
           }
         });
     });
+  }
+
+  createFile(data: ApiMetricsByTribeResponse[]): Readable {
+    const csvContent = convertFromApiMetricsByTribeResponseToCsvAsString(data);
+    const readable = Readable.from([csvContent]);
+    readable.setEncoding('utf-8');
+    return readable;
   }
 }
